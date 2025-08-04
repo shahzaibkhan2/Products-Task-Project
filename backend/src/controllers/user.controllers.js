3c2c993a45db0e5ca46bc61e989b6e4d601819bc
Shahzaib Khan <drshahzeb47@gmail.com>
2025-08-04 17:36:21 +0500

    Products Additions

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -6,105 +6,6 @@ import { SafeError } from "../utils/safeError.js";
 import Product from "../models/product.models.js";
 import { SafeResponse } from "../utils/safeResponse.js";
 
-const signUp = SafeAsync(async (req, res) => {
-  const { email, fullname, password } = req?.body;
-  if (!email || !fullname || !password)
-    return res.status(400).json(
-      new SafeError(400, req.t("all_fields_required"), {
-        code: 400,
-        message: req.t("all_fields_required"),
-      })
-    );
-
-  const isUserExists = await User.findOne({ email });
-  if (isUserExists)
-    return res.status(401).json(
-      new SafeError(401, req.t("profile_already_exists"), {
-        code: 401,
-        message: req.t("profile_already_exists"),
-      })
-    );
-  const newUser = await User.create({
-    fullname,
-    email,
-    password,
-  });
-  if (newUser) {
-    const accessToken = await generateAccessToken(newUser?._id);
-    return res
-      .status(201)
-      .cookie("accessToken", accessToken, {
-        httpOnly: true,
-        samSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV !== "development",
-      })
-      .json(
-        new SafeResponse(201, {
-          code: 200,
-          message: req.t("user_signup_successfully"),
-          data: {
-            id: newUser?._id,
-            fullname: newUser?.fullname,
-            email: newUser?.email,
-            avatar: newUser?.avatar || "",
-          },
-        })
-      );
-  }
-});
-
-const login = SafeAsync(async (req, res) => {
-  const { email, password } = req?.body;
-  if (!email || !password || email.trim() === "" || password.trim() === "")
-    return res.status(400).json(
-      new SafeError(400, req.t("all_fields_required"), {
-        code: 400,
-        message: req.t("all_fields_required"),
-      })
-    );
-
-  const user = await User.findOne({ email });
-  if (!user)
-    return res.status(401).json(
-      new SafeError(401, req.t("invalid_credentials"), {
-        code: 401,
-        message: req.t("invalid_credentials"),
-      })
-    );
-  const isPassCorrect = await user.isPasswordCorrect(password);
-  if (isPassCorrect) {
-    const accessToken = await generateAccessToken(user?._id);
-    return res
-      .status(200)
-      .cookie("accessToken", accessToken, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV !== "development",
-      })
-      .json(
-        new SafeResponse(200, {
-          code: 200,
-          message: req.t("login_successful"),
-          data: {
-            id: user?._id,
-            fullname: user?.fullname,
-            email: user?.email,
-            avatar: user?.avatar || "",
-          },
-        })
-      );
-  } else {
-    return res.status(400).json(
-      new SafeError(400, req.t("password_incorrect"), {
-        code: 400,
-        message: req.t("password_incorrect"),
-      })
-    );
-  }
-});
-
 const addProduct = SafeAsync(async (req, res) => {
   const { name, price, quantity, description, email, username } = req.body;
   const product = await Product.create({ name, price, quantity, description, email, username });
@@ -210,16 +111,16 @@ const createUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    // Optional: Check if user already exists
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Create new user
+ 
     const newUser = await User.create({ username, email, password });
 
-    // Optional: remove password before sending response
+
     const userResponse = newUser.toObject();
     delete userResponse.password;
 
@@ -239,4 +140,4 @@ const createUser = async (req, res) => {
 
 
 
-export { signUp, login, getProductsWithPagination, addProduct, addToCart, createUser };
+export { getProductsWithPagination, addProduct, addToCart, createUser };
--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,10 +1,7 @@
 import { Router } from "express";
-import { addProduct, addToCart, createUser, getProductsWithPagination, login, signUp } from "../controllers/user.controllers.js";
+import { addProduct, addToCart, createUser, getProductsWithPagination } from "../controllers/user.controllers.js";
 
 const router = Router();
-
-// router.route("/signup").post(signUp);
-// router.route("/login").post(login);
 
 router.route("/add-product").post(addProduct);
 router.route("/get-products").get(getProductsWithPagination);
